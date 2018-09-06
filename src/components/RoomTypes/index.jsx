92a4b7863830cdc4d8d44216d241eb8a63fc2d99
Jirka Chadima <chadima.jiri@gmail.com>
2018-09-06 15:18:00 +0200

    feat: Adds base estimate to hotel detail list of room types

--- src/components/RoomTypes/index.jsx
+++ src/components/RoomTypes/index.jsx
@@ -6,28 +6,52 @@ import AmenitiesList from '../AmenitiesList';
 
 // TODO use properties, totalQuantity and occupancy
 
-const RoomType = ({ roomType }) => (
+const RoomType = ({ roomType, estimate }) => (
   <div className="card">
     <img className="card-img-top" src={roomType.images[0]} alt={roomType.images[0]} />
     <div className="card-body">
       <h5 className="card-title">{roomType.name}</h5>
       <ReactMarkdown className="card-text" source={roomType.description} />
+      {estimate.price && (
+      <p>
+        <strong>
+  Available from
+          {' '}
+          {estimate.price}
+          {' '}
+          {estimate.currency}
+        </strong>
+      </p>
+      )}
       <AmenitiesList list={roomType.amenities} />
     </div>
   </div>
 );
 
-RoomType.propTypes = {
-  roomType: PropTypes.instanceOf(Object).isRequired,
+RoomType.defaultProps = {
+  estimate: {},
 };
 
-const RoomTypes = ({ map }) => {
-  const roomTypes = map && Object.values(map).map(rt => <RoomType key={rt.id} roomType={rt} />);
+RoomType.propTypes = {
+  roomType: PropTypes.instanceOf(Object).isRequired,
+  estimate: PropTypes.instanceOf(Object),
+};
+
+const RoomTypes = ({ map, estimates }) => {
+  const roomTypes = map && Object.values(map)
+    .map(rt => (
+      <RoomType
+        key={rt.id}
+        roomType={rt}
+        estimate={estimates.find(e => e.id === rt.id)}
+      />
+    ));
   return roomTypes && <div>{roomTypes}</div>;
 };
 
 RoomTypes.propTypes = {
   map: PropTypes.instanceOf(Object).isRequired,
+  estimates: PropTypes.instanceOf(Array).isRequired,
 };
 
 export default RoomTypes;
