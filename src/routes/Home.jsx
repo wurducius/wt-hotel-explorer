012a4b928febc3a5994fea2f22dc5b09606203af
Pablo Alice <pabloalice95@gmail.com>
2018-09-07 16:32:20 -0300

    style(routes/Home.jsx): fix code style

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -25,20 +25,20 @@ class Home extends React.PureComponent {
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-          {!areHotelsInitialized
-            ? <Loader block={200} label="Loading hotels from API..." />
-            : (
-              <React.Fragment>
-                <GuestForm handleSubmit={handleGuestFormSubmit} />
-                <HotelListing
-                  hotels={hotels || []}
-                  estimates={estimates || {}}
-                  isLoadingMore={isLoadingMore}
-                  showMore={!!next}
-                  fetchMoreHotels={fetchHotelsData}
-                />
-              </React.Fragment>
-            )}
+            {!areHotelsInitialized
+              ? <Loader block={200} label="Loading hotels from API..." />
+              : (
+                <React.Fragment>
+                  <GuestForm handleSubmit={handleGuestFormSubmit} />
+                  <HotelListing
+                    hotels={hotels || []}
+                    estimates={estimates || {}}
+                    isLoadingMore={isLoadingMore}
+                    showMore={!!next}
+                    fetchMoreHotels={fetchHotelsData}
+                  />
+                </React.Fragment>
+              )}
           </div>
         </div>
       </div>
