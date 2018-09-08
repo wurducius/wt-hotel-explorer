bce185de1da3fdf9691787b9f560567c6879fd2e
Jirka Chadima <chadima.jiri@gmail.com>
2018-09-08 18:47:48 +0200

    test(estimates): Adds more tests, make the older tests actually work

--- test/actions/estimates.spec.jsx
+++ test/actions/estimates.spec.jsx
@@ -5,109 +5,125 @@ describe('action:estimates', () => {
     let dispatchMock;
     let getStateMock;
     let exampleState;
+    let action;
 
     beforeEach(() => {
       dispatchMock = jest.fn();
       getStateMock = jest.fn();
       exampleState = {
         estimates: {
-          arrival: '2018-01-01',
-          departure: '2018-04-01',
-          numberOfGuests: 2,
+          guestData: {
+            arrival: '2018-01-01',
+            departure: '2018-04-01',
+            numberOfGuests: 2,
+          },
         },
         hotels: {
           list: [{
             id: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
-            ratePlans: [],
-            roomTypes: [],
+            currency: 'CZK',
+            ratePlans: {
+              rpa: {
+                id: 'rpa',
+                roomTypeIds: [],
+              },
+            },
+            roomTypes: {
+              rta: {
+                id: 'rta',
+              },
+            },
           }],
         },
       };
+      action = estimatesActions.recomputeHotelEstimates({ id: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3' });
     });
 
-    it('should not do anything when hotel does not exist', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.hotels.list = [];
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
+    describe('input validation', () => {
+      it('should not do anything when hotel does not exist', () => {
+        exampleState.hotels.list = [];
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
+
+      it('should not do anything when hotel does not have roomTypes', () => {
+        exampleState.hotels.list = [{
+          id: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
+          ratePlans: {},
+        }];
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
+
+      it('should not do anything when hotel does not have ratePlans', () => {
+        exampleState.hotels.list = [{
+          id: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
+          roomTypes: {},
+        }];
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
+
+      it('should not do anything when there are no guest data', () => {
+        exampleState.estimates = {};
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
+
+      it('should not do anything when numberOfGuests is missing', () => {
+        exampleState.estimates.guestData = {
+          arrival: '2018-01-01',
+          departure: '2018-04-01',
+        };
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
+
+      it('should not do anything when arrival is missing', () => {
+        exampleState.estimates.guestData = {
+          departure: '2018-01-01',
+          numberOfGuests: 1,
+        };
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
+
+      it('should not do anything when departure is missing', () => {
+        exampleState.estimates.guestData = {
+          arrival: '2018-01-01',
+          numberOfGuests: 1,
+        };
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(0);
+        expect(getStateMock.mock.calls.length).toBe(1);
+      });
     });
 
-    it('should not do anything when hotel does not have roomTypes', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.hotels.list = [{
-        id: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
-        ratePlans: [],
-      }];
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
-    });
-
-    it('should not do anything when hotel does not have ratePlans', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.hotels.list = [{
-        id: '0x933198455e38925bccb4bfe9fb59bac31d00b4d3',
-        roomTypes: [],
-      }];
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
-    });
-
-    it('should not do anything when there are no guest data', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.estimates = {};
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
-    });
-
-    it('should not do anything when numberOfGuests is missing', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.estimates.guestData = {
-        arrival: '2018-01-01',
-        arrival: '2018-04-01',
-      };
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
-    });
-
-    it('should not do anything when arrival is missing', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.estimates.guestData = {
-        departure: '2018-01-01',
-        numberOfGuests: 1
-      };
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
-    });
-
-    it('should not do anything when departure is missing', () => {
-      const action = estimatesActions.recomputeHotelEstimates('0x933198455e38925bccb4bfe9fb59bac31d00b4d3');
-      exampleState.estimates.guestData = {
-        arrival: '2018-01-01',
-        numberOfGuests: 1
-      };
-      getStateMock.mockReturnValue(exampleState);
-      const returnValue = action(dispatchMock, getStateMock);
-      expect(returnValue).toBe(undefined);
-      expect(dispatchMock.mock.calls.length).toBe(0);
-      expect(getStateMock.mock.calls.length).toBe(1);
+    describe('rate plan computation', () => {
+      it('should return null price if no rate plan matches the room type', () => {
+        getStateMock.mockReturnValue(exampleState);
+        action(dispatchMock, getStateMock);
+        expect(dispatchMock.mock.calls.length).toBe(1);
+        expect(getStateMock.mock.calls.length).toBe(1);
+        expect(dispatchMock.mock.calls[0][0].payload).toHaveProperty('id', exampleState.hotels.list[0].id);
+        expect(dispatchMock.mock.calls[0][0].payload).toHaveProperty('data');
+        expect(dispatchMock.mock.calls[0][0].payload.data.filter(e => e.id === 'rta').length).toBe(1);
+        expect(dispatchMock.mock.calls[0][0].payload.data.find(e => e.id === 'rta')).toHaveProperty('price', undefined);
+        expect(dispatchMock.mock.calls[0][0].payload.data.find(e => e.id === 'rta')).toHaveProperty('currency', exampleState.hotels.list[0].currency);
+      });
     });
   });
 });
