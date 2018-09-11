cc053ac59db64885660ef8aac18ef4ddf44bc8e1
Nico Calabrese <nico.sikker@gmail.com>
2018-09-10 22:18:05 -0300

    fix: Display marker icon and images on map.
    #1

--- src/components/HotelLocation/index.jsx
+++ src/components/HotelLocation/index.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  Map, TileLayer, Marker, Popup,
-} from 'react-leaflet';
 
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
 
 class LocationMap extends React.PureComponent {
   state = {
-    zoom: 35,
+    zoom: 19,
   };
 
   render() {
     const { location, name, address } = this.props;
     const { zoom } = this.state;
-    const position = [location.latitude, location.longitude];
+    const position = [location.latitude, location.longitude]; // TEST [52.531015, 13.384402];
+
+    // Set path to marker icon
+    delete L.Icon.Default.prototype._getIconUrl;
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+      iconUrl: require('leaflet/dist/images/marker-icon.png'),
+      shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+    });
 
     return (
-      <Map center={position} zoom={zoom} style={{ height: 300 }}>
+      <Map center={position} zoom={zoom}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
