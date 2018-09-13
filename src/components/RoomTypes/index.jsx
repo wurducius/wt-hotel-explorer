ffc2c02d92e99eaa5705e5409712c9de892dd094
Nico Calabrese <nico.sikker@gmail.com>
2018-09-13 13:22:22 -0300

    fix(room-cards): Removed duplicated className that affected image container height. Corrected card h

--- src/components/RoomTypes/index.jsx
+++ src/components/RoomTypes/index.jsx
@@ -26,21 +26,19 @@ const RoomType = ({
       <div className={`col-sm-12 col-md-6 col-lg-${computedColSize} d-flex`}>
         <ScrollAnimation animateIn="fadeInUp" animateOnce delay={100 * index} className="w-100 d-flex">
           <div className="card mb-2">
-            <div className="card-img-top">
-              <button className="card-img-top area-btn" type="button" data-toggle="modal" data-target={`#roomModal-${index + 1}`}>
-                <div className="img-crop" style={{ backgroundImage: `URL(${roomType.images[0]})` }}>
-                  <img src={roomType.images[0]} alt={roomType.images[0]} />
-                </div>
+            <button className="card-img-top area-btn" type="button" data-toggle="modal" data-target={`#roomModal-${index + 1}`}>
+              <div className="img-crop" style={{ backgroundImage: `URL(${roomType.images[0]})` }}>
+                <img src={roomType.images[0]} alt={roomType.images[0]} />
+              </div>
 
-                <div className="area-btn__btn">
-                  <i className="mdi mdi-18px text-white mdi-arrow-expand" />
-                  <span className="d-none">View Photos</span>
-                </div>
-              </button>
-            </div>
+              <div className="area-btn__btn">
+                <i className="mdi mdi-18px text-white mdi-arrow-expand" />
+                <span className="d-none">View Photos</span>
+              </div>
+            </button>
 
-            <div className="card-body text-muted" style={{ minHeight: 200 }}>
-              <h5 className="card-title">{roomType.name}</h5>
+            <div className="card-body pt-1 text-muted" style={{ minHeight: 200 }}>
+              <h5 className="card-title h6">{roomType.name}</h5>
               <ReactMarkdown className="card-text" source={roomType.description} />
             </div>
 
