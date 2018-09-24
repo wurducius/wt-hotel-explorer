a171fe7b2cb7666e7242effb57474ef21be7032e
Jirka Chadima <chadima.jiri@gmail.com>
2018-09-24 12:35:30 +0200

    refactor(pricing): Moves pricing algorithm one level down

--- test/services/pricing-algorithm/index.spec.jsx
+++ test/services/pricing-algorithm/index.spec.jsx
@@ -1,8 +1,8 @@
 import dayjs from 'dayjs';
 import currency from 'currency.js';
-import pricingAlgorithm from '../../src/services/pricing-algorithm';
+import pricingAlgorithm from '../../../src/services/pricing-algorithm';
 
-describe('services.pricing-algorithm', () => {
+describe('services.pricing-algorithm.index', () => {
   let guestData;
   let hotel;
 
