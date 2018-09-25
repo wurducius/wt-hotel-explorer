0e9ddfef034dc4b030979645cc6bdacda2d7d136
Jirka Chadima <chadima.jiri@gmail.com>
2018-09-25 16:36:31 +0200

    feat(networking): Properly handles errors on a hotel list

--- src/reducers/errors.jsx
+++ src/reducers/errors.jsx
@@ -14,6 +14,13 @@ const reducer = (state = defaultState, action) => {
           message: action.payload.message,
         },
       });
+    case 'FETCH_LIST_FAILED':
+      return Object.assign({}, state, {
+        errorPage: {
+          status: action.payload.status,
+          message: action.payload.message,
+        },
+      });
     case 'FETCH_HOTEL_ROOM_TYPES_FAILED':
     case 'FETCH_HOTEL_RATE_PLANS_FAILED':
     case 'FETCH_HOTEL_AVAILABILITY_FAILED':
