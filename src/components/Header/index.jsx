48be5aba256f237f92657185cfc63af827f5d4d5
Nico Calabrese <nico.sikker@gmail.com>
2018-09-28 14:26:22 -0300

    feat(header): Added items to main navbar
    Now the header includes a link to go to the homepage, and a button to display the estimates form.
    
    #27

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+var is_root = location.pathname == "/";
+
 export default () => (
   <div id="app-header">
     <nav className="navbar navbar-expand-xl navbar-light" id="navbar">
       <div className="container">
         <Link className="navbar-brand mr-2" to="/">Winding Tree</Link>
+
+        <div className="collapse navbar-collapse" id="navbar-content">
+          <ul className="navbar-nav ml-auto" id="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link h5" to="/">Browse Hotels</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="ml-lg-1">
+          <button className="btn btn-block btn-primary" id="navbar-btn" type="button" data-toggle="collapse" data-target="#form-estimates" aria-expanded="false" aria-controls="form-estimates">
+            Get Estimates!
+          </button>
+        </div>
       </div>
     </nav>
   </div>
