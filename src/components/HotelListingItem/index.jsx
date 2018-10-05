84172a2b6e3f912c2d08e369dbef00aa3ed1e36e
Jirka Chadima <chadima.jiri@gmail.com>
2018-10-05 08:47:00 +0200

    fix(52): Uses div instead of Link for a HotelListingItem

--- src/components/HotelListingItem/index.jsx
+++ src/components/HotelListingItem/index.jsx
@@ -1,53 +1,79 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import ScrollAnimation from 'react-animate-on-scroll';
+import { Redirect } from 'react-router';
 
-const goToTop = () => {
-  window.scrollTo(0, 0);
-};
+class HotelListingItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      goToDetail: false,
+    };
+    this.goToTop = this.goToTop.bind(this);
+    this.goToTopWithKeyboard = this.goToTopWithKeyboard.bind(this);
+  }
 
-const HotelListingItem = ({ hotel, estimates }) => {
-  const currentLowestEstimate = estimates.reduce((acc, current) => {
-    if (!acc.price || current.price <= acc.price) {
-      return current;
+  goToTop() {
+    this.setState({ goToDetail: true });
+    window.scrollTo(0, 0);
+  }
+
+  goToTopWithKeyboard(event) {
+    if (event.key === 'Enter') {
+      this.setState({ goToDetail: true });
+      window.scrollTo(0, 0);
     }
-    return acc;
-  }, {});
-  return (
-    <ScrollAnimation animateIn="fadeInUp" animateOnce>
+  }
 
-      <Link to={`/hotels/${hotel.id}`} onClick={goToTop} className="card mb-2">
-        <img src={hotel.images[0]} alt={hotel.name} className="card-img-top" />
-        <div className="card-body pt-1 text-muted block-fade">
-          <h5 className="card-title h6">{hotel.name}</h5>
-          <div className="card-text">
-            <ReactMarkdown source={hotel.description} />
+  render() {
+    const { hotel, estimates } = this.props;
+    const { goToDetail } = this.state;
+
+    if (goToDetail === true) {
+      return (<Redirect to={`/hotels/${hotel.id}`} />);
+    }
+
+    const currentLowestEstimate = estimates.reduce((acc, current) => {
+      if (!acc.price || current.price <= acc.price) {
+        return current;
+      }
+      return acc;
+    }, {});
+    return (
+      <ScrollAnimation animateIn="fadeInUp" animateOnce>
+
+        <div onClick={this.goToTop} onKeyPress={this.goToTopWithKeyboard} className="card mb-2 card-with-links" role="link" tabIndex="0">
+          <img src={hotel.images[0]} alt={hotel.name} className="card-img-top" />
+          <div className="card-body pt-1 text-muted block-fade">
+            <h5 className="card-title h6">{hotel.name}</h5>
+            <div className="card-text">
+              <ReactMarkdown source={hotel.description} />
+            </div>
           </div>
-        </div>
-        {currentLowestEstimate.price && (
-        <div className="card-footer bg-white pt-0">
-          <div className="animated fadeIn text--accent">
-            <i className="mdi mdi-calendar mdi-18px text-muted" />
-            {' '}
-            <strong>
+          {currentLowestEstimate.price && (
+          <div className="card-footer bg-white pt-0">
+            <div className="animated fadeIn text--accent">
+              <i className="mdi mdi-calendar mdi-18px text-muted" />
+              {' '}
+              <strong>
                   Available from
-              {' '}
-              <span className="font--alt">{currentLowestEstimate.price.format()}</span>
-              {' '}
-              {currentLowestEstimate.currency}
-            </strong>
+                {' '}
+                <span className="font--alt">{currentLowestEstimate.price.format()}</span>
+                {' '}
+                {currentLowestEstimate.currency}
+              </strong>
+            </div>
+          </div>
+          )}
+          <div className="card-footer bg-white pt-0">
+            <span className="text--link border-bottom">See detail</span>
           </div>
         </div>
-        )}
-        <div className="card-footer bg-white pt-0">
-          <span className="text--link border-bottom">See detail</span>
-        </div>
-      </Link>
-    </ScrollAnimation>
-  );
-};
+      </ScrollAnimation>
+    );
+  }
+}
 
 HotelListingItem.defaultProps = {
   estimates: [],
