d1d6d9f5fbc0d0a59604d543b49a81c9fa646c3c
Pablo Alice <pabloalice95@gmail.com>
2018-10-10 11:37:40 -0300

    refactor(Header): take location from router instead of from window

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router-dom';
 
-export default () => (
-  <div id="app-header">
-    <nav className="navbar navbar-expand-xl navbar-light" id="navbar">
-      <div className="container">
-        <Link className="navbar-brand mr-2" to="/">Winding Tree</Link>
-        {/hotels\/.*/.test(window.location.pathname)
-        && (
-        <div className="collapse navbar-collapse" id="navbar-content">
-          <ul className="navbar-nav ml-auto" id="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link h5" to="/">Browse Hotels</Link>
-            </li>
-          </ul>
+const Header = (props) => {
+  const { location } = props;
+  const showBrowseHotels = /hotels\/.+/.test(location.pathname);
+  return (
+    <div id="app-header">
+      <nav className="navbar navbar-expand-xl navbar-light" id="navbar">
+        <div className="container">
+          <Link className="navbar-brand mr-2" to="/">Winding Tree</Link>
+          {showBrowseHotels
+          && (
+          <div className="collapse navbar-collapse" id="navbar-content">
+            <ul className="navbar-nav ml-auto" id="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link h5" to="/">Browse Hotels</Link>
+              </li>
+            </ul>
+          </div>
+          )
+          }
+          <div className="ml-lg-1">
+            <button className="btn btn-block btn-primary" id="navbar-btn" type="button" data-toggle="collapse" data-target="#form-estimates" aria-expanded="false" aria-controls="form-estimates">
+              Get Estimates!
+            </button>
+          </div>
         </div>
-        )}
+      </nav>
+    </div>
+  );
+};
 
-        <div className="ml-lg-1">
-          <button className="btn btn-block btn-primary" id="navbar-btn" type="button" data-toggle="collapse" data-target="#form-estimates" aria-expanded="false" aria-controls="form-estimates">
-            Get Estimates!
-          </button>
-        </div>
-      </div>
-    </nav>
-  </div>
-);
+Header.propTypes = {
+  location: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default withRouter(Header);
