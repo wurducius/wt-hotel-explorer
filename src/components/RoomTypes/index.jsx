bfb20704951389ce25a7742bdf5373c114f6489f
Martin Novák <martin@martinnovak.net>
2018-10-25 12:54:51 +0200

    Imrpoving code to show qr code and send some data to siesta server

--- src/components/RoomTypes/index.jsx
+++ src/components/RoomTypes/index.jsx
@@ -10,7 +10,7 @@ const QuantityBadge = ({ quantity }) => {
   if (quantity === 0) {
     return (
       <React.Fragment>
-        <i className="mdi mdi-close-octagon text-danger" />
+        <i className="mdi mdi-close-octagon text-danger"/>
         {' '}
         <em>Sold out!</em>
       </React.Fragment>);
@@ -18,21 +18,21 @@ const QuantityBadge = ({ quantity }) => {
   if (quantity < 3) {
     return (
       <React.Fragment>
-        <i className="mdi mdi-alert-octagram text-warning" />
+        <i className="mdi mdi-alert-octagram text-warning"/>
         {' '}
         <em>
-Last
+          Last
           {' '}
           {quantity}
           {' '}
-remaining!
+          remaining!
         </em>
       </React.Fragment>);
   }
   if (quantity === undefined) {
     return (
       <React.Fragment>
-        <i className="mdi mdi-alert-circle-outline text-muted" />
+        <i className="mdi mdi-alert-circle-outline text-muted"/>
         {' '}
         <em>Availability unknown</em>
       </React.Fragment>);
@@ -61,13 +61,106 @@ class RoomType extends React.PureComponent {
     if (estimate.quantity === 0) {
       availabilityClassNames = 'text-muted text--deleted';
     }
+
+    const qrStyle = {
+      display: 'block',
+      visibility: 'visible',
+    };
+
+    function book() {
+      const request = new XMLHttpRequest();
+
+      const name = document.getElementById('firstName').value;
+      const surname = document.getElementById('lastname').value;
+      const email = document.getElementById('email').value;
+
+      const data = {
+        hotelId: '0xe92a8f9a7264695f4aed8d1f397dbc687ba40299',
+        customer: {
+          name,
+          surname,
+          address: {
+            line1: '221B Baker Street',
+            city: 'London',
+            country: 'GB',
+          },
+          email,
+        },
+        pricing: {
+          currency: 'GBP',
+          total: 221,
+          cancellationFees: [
+            {
+              from: '2018-12-01',
+              to: '2019-01-01',
+              amount: 50,
+            },
+          ],
+        },
+        booking: {
+          arrival: '2019-01-01',
+          departure: '2019-01-03',
+          rooms: [
+            {
+              id: '314',
+              guestInfoIds: ['1'],
+            },
+            {
+              id: '315',
+              guestInfoIds: ['2'],
+            },
+          ],
+          guestInfo: [
+            {
+              id: '1',
+              name: 'Sherlock',
+              surname: 'Holmes',
+            },
+            {
+              id: '2',
+              name: 'John',
+              surname: 'Watson',
+            },
+          ],
+        },
+      };
+      const dataStr = JSON.stringify(data);
+
+      request.open('POST', 'https://stage.siestasolution.com/Api/Api.aspx?Action=WTBooking', true);
+      request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+      console.log('POST:', data);
+      request.send(dataStr);
+
+      console.log('Testing QR Code');
+
+      const qr = 'TEST';
+      const qrContainer = document.getElementById('qr-container');
+      const qrImage = document.getElementById('qr-image');
+      qrImage.src = `https://chart.googleapis.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl=${qr}`;
+      qrContainer.style.display = 'block';
+      qrContainer.style.visibility = 'visible';
+    }
+
     return (
       <React.Fragment>
         <div className="col-sm-12 col-md-6 col-lg-4 d-flex">
-          <ScrollAnimation animateIn="fadeInUp" animateOnce delay={100 * index} className="w-100 d-flex">
+          <ScrollAnimation
+            animateIn="fadeInUp"
+            animateOnce
+            delay={100 * index}
+            className="w-100 d-flex"
+          >
             <div className="card mb-2">
-              <button className="card-img-top area-btn" type="button" data-toggle="modal" data-target={`#roomModal-${index + 1}`}>
-                <div className="img-crop" style={{ backgroundImage: `URL(${roomType.images && roomType.images[0]})` }}>
+              <button
+                className="card-img-top area-btn"
+                type="button"
+                data-toggle="modal"
+                data-target={`#roomModal-${index + 1}`}
+              >
+                <div
+                  className="img-crop"
+                  style={{ backgroundImage: `URL(${roomType.images && roomType.images[0]})` }}
+                >
                   <img
                     src={roomType.images && roomType.images[0]}
                     alt={roomType.images && roomType.images[0]}
@@ -75,38 +168,56 @@ class RoomType extends React.PureComponent {
                 </div>
 
                 <div className="area-btn__btn">
-                  <i className="mdi mdi-18px text-white mdi-arrow-expand" />
+                  <i className="mdi mdi-18px text-white mdi-arrow-expand"/>
                   <span className="d-none">View Photos</span>
                 </div>
               </button>
 
               <div className="card-body pt-1 text-muted" style={{ minHeight: 200 }}>
                 <h5 className="card-title h6">{roomType.name}</h5>
-                <ReactMarkdown className="card-text text--weight-normal" source={roomType.description} />
+                <ReactMarkdown
+                  className="card-text text--weight-normal"
+                  source={roomType.description}
+                />
               </div>
 
               {estimate.price && (
-              <div className="room-price card-footer bg-white pb-0">
-                <div className="animated fadeIn">
-                  <p>
-                    <i className="mdi mdi-calendar mdi-18px text-muted" />
-                    {' '}
-                    <strong className={availabilityClassNames}>
-                  Available from
+                <div className="room-price card-footer bg-white pb-0">
+                  <div className="animated fadeIn">
+                    <div>
+                      <i className="mdi mdi-calendar mdi-18px text-muted"/>
                       {' '}
-                      <span className="font--alt">{estimate.price.format()}</span>
-                      {' '}
-                      {estimate.currency}
-                    </strong>
-			<button onClick={ book() }>Book!</button>
-                  </p>
-                  {estimate.price && (<QuantityBadge quantity={estimate.quantity} />)}
+                      <strong className={availabilityClassNames}>
+                        Available from
+                        {' '}
+                        <span className="font--alt">{estimate.price.format()}</span>
+                        {' '}
+                        {estimate.currency}
+                      </strong>
+                      <br/>
+                      <label htmlFor="firstName">First name:</label>
+                      <input id="firstName" name="firstName" />
+                      <br/>
+                      <label htmlFor="lastname">Last name:</label>
+                      <input id="lastname" name="lastname" />
+                      <br/>
+                      <label htmlFor="name">Email:</label>
+                      <input id="email" name="email" />
+                      <br/>
+                      <button onClick={book}>Book!</button>
+                      <br/>
+                      <div id="qr-container">
+                        <b>Please, pay to:</b>
+                        <img style={qrStyle} id="qr-image"/>
+                      </div>
+                    </div>
+                    {estimate.price && (<QuantityBadge quantity={estimate.quantity}/>)}
+                  </div>
                 </div>
-              </div>
               )}
               {roomType.amenities && (
                 <div className="card-footer bg-white">
-                  <AmenitiesList list={roomType.amenities} />
+                  <AmenitiesList list={roomType.amenities}/>
                 </div>
               )}
             </div>
@@ -114,17 +225,27 @@ class RoomType extends React.PureComponent {
         </div>
 
         {/* Modal */}
-        <div className="modal modal--carousel" id={`roomModal-${index + 1}`} tabIndex={`-${index + 1}`} role="dialog">
+        <div
+          className="modal modal--carousel"
+          id={`roomModal-${index + 1}`}
+          tabIndex={`-${index + 1}`}
+          role="dialog"
+        >
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title animated fadeIn">{roomType.name}</h5>
-                <button type="button" className="close animated fadeIn" data-dismiss="modal" aria-label="Close">
-                  <i className="mdi mdi-close" />
+                <button
+                  type="button"
+                  className="close animated fadeIn"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <i className="mdi mdi-close"/>
                 </button>
               </div>
               <div className="modal-body d-flex align-items-center animated fadeIn">
-                <ImageList list={roomType.images} withIndicators />
+                <ImageList list={roomType.images} withIndicators/>
               </div>
             </div>
           </div>
@@ -132,19 +253,6 @@ class RoomType extends React.PureComponent {
       </React.Fragment>
     );
   }
-}
-
-function book(){
-	var request = new XMLHttpRequest()
-
-
-	var data = "{\r\n  \"hotelId\": \"0xe92a8f9a7264695f4aed8d1f397dbc687ba40299\",\r\n  \"customer\": {\r\n    \"name\": \"Sherlock\",\r\n    \"surname\": \"Holmes\",\r\n    \"address\": {\r\n      \"line1\": \"221B Baker Street\",\r\n      \"city\": \"London\",\r\n      \"country\": \"GB\"\r\n    },\r\n    \"email\": \"sherlock.holmes@houndofthebaskervilles.net\"\r\n  },\r\n  \"pricing\": {\r\n    \"currency\": \"GBP\",\r\n    \"total\": 221,\r\n    \"cancellationFees\": [\r\n      { \"from\": \"2018-12-01\", \"to\": \"2019-01-01\", \"amount\": 50 }\r\n    ]\r\n  },\r\n  \"booking\": {\r\n    \"arrival\": \"2019-01-01\",\r\n    \"departure\": \"2019-01-03\",\r\n    \"rooms\": [\r\n      {\r\n        \"id\": \"314\",\r\n        \"guestInfoIds\": [\"1\"]\r\n      },\r\n      {\r\n        \"id\": \"315\",\r\n        \"guestInfoIds\": [\"2\"]\r\n      }\r\n    ],\r\n    \"guestInfo\": [\r\n      {\r\n        \"id\": \"1\",\r\n        \"name\": \"Sherlock\",\r\n        \"surname\": \"Holmes\"\r\n      },\r\n      {\r\n        \"id\": \"2\",\r\n        \"name\": \"John\",\r\n        \"surname\": \"Watson\"\r\n      }\r\n    ]\r\n  }\r\n}"
-
-
-	request.open('POST', 'https://stage.siestasolution.com/Api/Api.aspx?Action=WTBooking', true)
-	request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8')
-	console.log("POST: "+data)
-	request.send(data)
 }
 
 RoomType.defaultProps = {
